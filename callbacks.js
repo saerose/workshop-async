b630dc2515c21ba1b8f33f493649f37c6d9205e9
Arol <arolet@gmail.com>
2017-04-08 12:25:56 +0200

    feat: a possible callbacks soluction

--- callbacks.js
+++ callbacks.js
@@ -2,31 +2,47 @@
 
 const fs = require('fs');
 
+exports.uploadFileContents = (data, cb) => {
+  // Simulating file upload
+  if(!data){
+    cb('No data provided');
+  }
+  setTimeout(() => {
+    cb(null)
+  }, 1000)
+}
+
+exports.uploadFile = (filePath, cb) => {
+  fs.readFile(filePath, function(err, data){
+    if (err) {
+      cb('Error reading file: ' + err)
+    } else {
+      exports.uploadFileContents(data, cb);
+    }
+  })
+}
+
+const uploadFileList = (source, files, cb) => {
+  const fileQueue = [];
+  files.forEach(function (filename, fileIndex) {
+    const filePath = source+'/'+filename
+    // Uploading the file
+    fileQueue.push(filePath);
+    exports.uploadFile(filePath, () => {
+      fileQueue.splice(fileQueue.indexOf(filePath), 1);
+      if(fileQueue.length === 0) {
+        cb(null, files.length)
+      }
+    })
+  })
+}
+
 exports.uploadFolder = (source, cb) => {
   fs.readdir(source, function (err, files) {
     if (err) {
       cb('Error finding files: ' + err)
     } else {
-      const fileQueue = [];
-
-      files.forEach(function (filename, fileIndex) {
-        const filePath = source+'/'+filename
-        // Uploading the file
-        fileQueue.push(filePath)
-        fs.readFile(filePath, function(err, data){
-          if (err) {
-            console.log('Error reading file: ' + err)
-          } else {
-            // Simulating file upload
-            setTimeout(() => {
-              fileQueue.splice(fileQueue.indexOf(filePath), 1);
-              if(fileQueue.length === 0) {
-                cb(null, files.length)
-              }
-            }, 1000)
-          }
-        })
-      })
+      uploadFileList(source, files, cb);
     }
   })
 }
